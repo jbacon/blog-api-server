55b4696aa912e2ed799a888d9551c984d3a87690
Bacon, Josh <Josh.L.Bacon@nordstrom.com>
2018-01-15 20:22:02 -0800

    Travis fixes? hopefully..

--- before_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-openssl aes-256-cbc -K $encrypted_82bd95a7f607_key -iv $encrypted_82bd95a7f607_iv -in id_rsa.enc -out id_rsa -d
-chmod 600 id_rsa
-eval "$(ssh-agent -s)"
-ssh-add id_rsa
-rm -f id_rsa
-openssl aes-256-cbc -K $encrypted_82bd95a7f607_key -iv $encrypted_82bd95a7f607_iv -in configs-prod.json.enc -out configs-prod.json -d
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+openssl aes-256-cbc -K $encrypted_82bd95a7f607_key -iv $encrypted_82bd95a7f607_iv -in id_rsa.enc -out id_rsa -d
+chmod 600 id_rsa
+eval "$(ssh-agent -s)"
+ssh-add id_rsa
+rm -f id_rsa
+openssl aes-256-cbc -K $encrypted_82bd95a7f607_key -iv $encrypted_82bd95a7f607_iv -in configs-prod.json.enc -out configs-prod.json -d
 ssh portfolio@portfolioapi.joshbacon.name docker-compose -f /home/portfolio/api-server/docker-compose-prod.yaml down --remove-orphans
 rsync -r --delete-after --quiet --exclude .git/ ${TRAVIS_BUILD_DIR}/ portfolio@portfolioapi.joshbacon.name:/home/portfolio/api-server/
 ssh portfolio@portfolioapi.joshbacon.name docker-compose -f /home/portfolio/api-server/docker-compose-prod.yaml up -d --remove-orphans --force-recreate
